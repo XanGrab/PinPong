9649e1391efce6e1a649c58be051456aa8cf7c03
ewjensen <ewjensen@wisc.edu>
2021-11-06 15:34:43 -0500

    Commited

---
+++ Demos/Simple-Pong/Assets/Scripts/Target.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  void OnCollisionEnter2D(Collision2D other) {
+Debug.Log("test");
+Debug.Log("test" + other.gameObject.tag);
+             if (other.gameObject.tag == "target1" || other.gameObject.tag == "target2" || other.gameObject.tag == "target3" || other.gameObject.tag == "target4" || other.gameObject.tag == "target5") {
+            Debug.Log ("pushRight");
+            Destroy(other.gameObject);
+            }
+            
+    }
+}
