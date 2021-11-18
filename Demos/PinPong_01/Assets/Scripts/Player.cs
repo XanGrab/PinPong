5df29ed5b34eda26e883b597169105a90d0604b8
unknown <ajgrabowski@wisc.edu>
2021-11-17 22:29:08 -0600

    Changing Color & Trail

--- Demos/PinPong_01/Assets/Scripts/Player.cs
+++ Demos/PinPong_01/Assets/Scripts/Player.cs
@@ -15,6 +15,7 @@ public class Player : MonoBehaviour
     private state playerState;
     public float speed;
     public float flipTorque;
+
     private Vector2 movVector;
     private bool flippingUp;
     private bool flippingDown;
